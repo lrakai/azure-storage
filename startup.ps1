b96b8bff8b0f4321e5ebda3c387a94f31b2f6bd4
Logan Rakai <lrakai@users.noreply.github.com>
2017-08-15 22:34:24 -0600

    Correct IP name

--- startup.ps1
+++ startup.ps1
@@ -2,4 +2,4 @@ $Region = "WestUS2"
 Login-AzureRmAccount
 New-AzureRmResourceGroup -Name azure-storage-lab -Location $Region
 New-AzureRmResourceGroupDeployment -ResourceGroupName azure-storage-lab -Name storage-lab -TemplateFile .\infrastructure\arm-template.json
-Get-AzureRmPublicIpAddress -Name lab-vm-ip -ResourceGroupName azure-storage-lab | Select -ExpandProperty IpAddress
+Get-AzureRmPublicIpAddress -Name ca-lab-vm-ip -ResourceGroupName azure-storage-lab | Select -ExpandProperty IpAddress
